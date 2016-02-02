166066a747ac7130b05d4db4b5a1968faf7517d9
Samir Moreira Antonio <samir.ma@gmail.com>
2016-02-01 23:18:37 -0200

    Starting working with view

--- app/src/main/java/com/samir/guessinggame/GuessingGameMainActivity.java
+++ app/src/main/java/com/samir/guessinggame/GuessingGameMainActivity.java
@@ -2,6 +2,8 @@ package com.samir.guessinggame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.samir.guessinggame.guessGame.GuessGameDelegate;
 import com.samir.guessinggame.guessGame.GuessingGame;
@@ -10,6 +12,7 @@ import com.samir.guessinggame.guessGame.GuessingGameFactory;
 public class GuessingGameMainActivity extends AppCompatActivity implements GuessGameDelegate {
 
     private GuessingGame guessingGame;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,8 @@ public class GuessingGameMainActivity extends AppCompatActivity implements Guess
         setContentView(R.layout.activity_guessing_game_main);
 
         guessingGame = GuessingGameFactory.getGuessingGame(this);
+
+        textView = (TextView) findViewById(R.id.id_text);
 
 
     }
@@ -30,11 +35,20 @@ public class GuessingGameMainActivity extends AppCompatActivity implements Guess
 
     @Override
     public void askForAttribute(String attribute) {
-
+        textView.setText(attribute + "?");
     }
 
     @Override
     public void takeAGuess(String animal) {
-
+        textView.setText(animal + "?");
     }
+
+    public void answerYes(View view) {
+        guessingGame.yes();
+    }
+
+    public void answerNo(View view) {
+        guessingGame.no();
+    }
+
 }
